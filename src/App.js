06301da6a2170ab9ef5f5fd819fa39027b3507cb
Ashwin Prabhu <ashwin.msprabhu@gmail.com>
2021-02-26 20:45:49 -0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [postTitle, setPostTitle] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setPosts(json));
+  });
+
+  const updatePostTitle = (index) => {
+    let selectedPostId = posts.findIndex((x) => x.id === index + 1);
+    setPostTitle(posts[selectedPostId].body);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: 60 }}>
+      <h3>Posts From JSONPlaceHolder</h3>
+      {posts &&
+        posts.map((post, index) => {
+          if (index < 5)
+            return (
+              <div
+                key={index}
+                style={{
+                  marginTop: 10,
+                  textDecorationLine: "underline",
+                  color: "blue",
+                }}
+                onClick={() => {
+                  updatePostTitle(index);
+                }}
+              >
+                {index + 1}. {post.title}
+              </div>
+            );
+        })}
+
+      <div style={{ marginTop: 20, textDecorationLine: "underline" }}>
+        Description of selected title:
+      </div>
+      <div style={{ width: 600 }}>{postTitle}</div>
     </div>
   );
 }
